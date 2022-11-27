49ce67a8f313a7c54cf2d5386921dfd89c9eae86
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-11-27 20:57:13 +0000

    Update Initialization.ps1

--- _psuedocode/Initialization.ps1
+++ _psuedocode/Initialization.ps1
@@ -10,14 +10,29 @@ function Initialization
         #>
     }
 
-
-#Teams install path 
+    #Teams install path. 
     {
         $TeamsPath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams')
         $TeamsUpdateExePath = [System.IO.Path]::Combine($TeamsPath, 'Update.exe')
     }
 
 
-if ($TeamsUpdateExePath - ) {
-    
+    #CheckPath
+    if ($TeamsUpdateExePath -and $TeamsPath -eq $true ) 
+    { 
+        Write-Host "Stopping Teams" -ForegroundColor Yellow
+        Get-Process -ProcessName Teams | Stop-Process -Force
+        Clear
+        Write-Host "Teams Stopped, please wait..." -ForegroundColor Green
+        Clear
+        Write-Host "Nuking Teams, please wait..." -ForegroundColor Green
+        Nuke-Teams -Wait
+        Write-Host "Nuked ; proceeding with reinstall + optimizations..." -ForegroundColor Green
+        Add-Teams -Wait
+    } 
+    else 
+    { 
+        Write-Host "No Install detected , proceeding with install + optimizations..." -ForegroundColor Green
+        Add-Teams -Wait
+    }
 }
